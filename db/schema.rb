0c0e091dfbf163f90d82a2b341a9ae0b929b4be6
Fernando Morgenstern <contato@fernandomarcelo.com>
2014-10-18 11:06:11 -0300

    Adding date field to links table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018140227) do
+ActiveRecord::Schema.define(version: 20141018140440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20141018140227) do
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "date"
   end
 
   add_index "links", ["promise_id"], name: "index_links_on_promise_id", using: :btree
