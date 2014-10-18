9e8e121c9bddfd41c731b9a0fde9b7c8d48c90c3
Fernando Morgenstern <contato@fernandomarcelo.com>
2014-10-18 11:03:36 -0300

    Adding date field to show when a promise was made.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018120411) do
+ActiveRecord::Schema.define(version: 20141018140227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20141018120411) do
     t.datetime "updated_at"
     t.boolean  "visible"
     t.string   "status"
+    t.date     "date"
   end
 
   add_index "promises", ["candidate_id"], name: "index_promises_on_candidate_id", using: :btree
