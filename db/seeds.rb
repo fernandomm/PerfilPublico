c11e7b66b62cbc05b75c836818566b82e2fcf2ba
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-18 17:23:47 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+unless Candidate.exists?
+  ActiveRecord::Base.transaction do
+    psdb = PoliticalParty.create(name: 'PSDB',
+      image: File.new('./spec/fixures/psdb.png'))
+    pt = PoliticalParty.create(name: 'PT',
+      image: File.new('./spec/fixures/pt.jpg'))
+    psdb.candidates.create(name: 'Aécio Neves', age: 54, biography: """
+      Aécio Neves da Cunha is a Brazilian economist, politician and president
+      of the Brazilian Social Democracy Party
+      """, image: File.new('./spec/fixures/aecio.jpg'))
+
+    pt.candidates.create(name: 'Dilma Rousseff', age: 66, biography: """
+      Dilma Vana Rousseff is the 36th and current President of Brazil. She is
+      the first woman to hold the office. She was previously the Chief of Staff
+      of the President Luiz Inácio Lula da Silva from 2005 to 2010.
+      """, image: File.new('./spec/fixures/dilma.jpg'))
+  end
+end
