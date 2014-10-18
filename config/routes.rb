52910aa22fbe95a7e7616a5be1674e4b67507a9e
Ivan Santos <pragmaticivan@gmail.com>
2014-10-18 19:05:28 -0300

    [promises] Generate the promise visualization page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'main#index'
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
 
   get 'candidates/:id', to: 'candidates#show', as: :candidate
+
+  get 'candidates/:id/promises/:promise_id', to: 'promises#show', as: :promise
+
 end
