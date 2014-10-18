56ed5b8ea7914ee9755b75639aeb06b191a4ba06
Fernando Morgenstern <contato@fernandomarcelo.com>
2014-10-18 00:11:19 -0300

    Adding image to candidate model.

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,6 @@
 class Candidate < ActiveRecord::Base
   validates_presence_of :name, :biography, :age
   validates :age, numericality: { greater_than_or_equal_to: 18, less_than_or_equal_to: 100 }
+
+  mount_uploader :image, CandidateImageUploader
 end
