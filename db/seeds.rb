1af4aa2936fc273e085266fda500d5267633f00a
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-19 13:04:34 -0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,88 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
-unless Candidate.exists?
-  ActiveRecord::Base.transaction do
-    psdb = PoliticalParty.create(name: 'PSDB',
-      image: File.new('./spec/fixures/psdb.png'))
-    pt = PoliticalParty.create(name: 'PT',
-      image: File.new('./spec/fixures/pt.jpg'))
-    psdb.candidates.create(name: 'Aécio Neves', age: 54, biography: """
-      Aécio Neves da Cunha is a Brazilian economist, politician and president
-      of the Brazilian Social Democracy Party.
-      """, image: File.new('./spec/fixures/aecio.jpg'),
-      political_office: 'president')
+# unless Candidate.exists?
+#   ActiveRecord::Base.transaction do
+#     psdb = PoliticalParty.create(name: 'PSDB',
+#       image: File.new('./spec/fixures/psdb.png'))
+#     pt = PoliticalParty.create(name: 'PT',
+#       image: File.new('./spec/fixures/pt.jpg'))
+#     psdb.candidates.create(name: 'Aécio Neves', age: 54, biography: """
+#       Aécio Neves da Cunha is a Brazilian economist, politician and president
+#       of the Brazilian Social Democracy Party.
+#       """, image: File.new('./spec/fixures/aecio.jpg'),
+#       political_office: 'president')
+#
+#     pt.candidates.create(name: 'Dilma Rousseff', age: 66, biography: """
+#       Dilma Vana Rousseff is the 36th and current President of Brazil. She is
+#       the first woman to hold the office. She was previously the Chief of Staff
+#       of the President Luiz Inácio Lula da Silva from 2005 to 2010.
+#       """, image: File.new('./spec/fixures/dilma.jpg'),
+#       political_office: 'president')
+#   end
+# end
 
-    pt.candidates.create(name: 'Dilma Rousseff', age: 66, biography: """
-      Dilma Vana Rousseff is the 36th and current President of Brazil. She is
-      the first woman to hold the office. She was previously the Chief of Staff
-      of the President Luiz Inácio Lula da Silva from 2005 to 2010.
-      """, image: File.new('./spec/fixures/dilma.jpg'),
-      political_office: 'president')
-  end
-end
+
+# Seeds for the competition
+PoliticalParty.create(name: 'House Targaryen').candidates
+  .create(name: 'Daenerys Targaryen', age: 18, biography: """
+    Daenerys of the House Targaryen, the First of Her Name;
+    Queen of Meereen;
+    Queen of the Andals, the Rhoynar and the First Men;
+    Lady Regnant of the Seven Kingdoms;
+    Protector of the Realm;
+    Khaleesi of the Great Grass Sea;
+    Breaker of Chains;
+    Mother of Dragons.
+    """, image: File.new('./spec/fixures/daenerys.jpg'),
+    political_office: 'president' )
+
+baratheonn = PoliticalParty.create(name: 'House Baratheonn')
+baratheonn.candidates.create(name: 'Joffrey Baratheonn', age: 19, biography: """
+    King Joffrey of House Baratheon, the First of His Name.
+    The King on the Iron Throne.
+    """, image: File.new('./spec/fixures/joffrey.jpg'),
+    political_office: 'president' )
+baratheonn.candidates.create(name: 'Tommen Baratheon', age: 12,
+    biography: "Tommen of the House Baratheon the First of His Name",
+    image: File.new('./spec/fixures/tommen.jpg'),
+    political_office: 'city_councilman' )
+
+lannister = PoliticalParty.create(name: 'House Lannister')
+lannister.candidates.create(name: 'Tyrion Lannister', age: 36, biography: """
+    Intelligent, witty and well-read, and shares his father's skill for business
+    and political maneuvering.
+    """, image: File.new('./spec/fixures/tyrion.jpg'),
+    political_office: 'senator')
+lannister.candidates.create(name: 'Cersei Lannister', age: 40, biography: """
+    Queen Regent; Lady Paramount of the Westerlands; Lady of Casterly Rock.
+    """, image: File.new('./spec/fixures/cercei.png'),
+    political_office: 'senator')
+
+
+PoliticalParty.create(name: 'House Tyrell').candidates
+  .create(name: 'Olenna Tyrell', age: 90, biography: "The Queen of Thorns.",
+    image: File.new('./spec/fixures/olenna.jpg'),
+    political_office: 'congressman' )
+
+stark = PoliticalParty.create(name: 'House Stark');
+stark.candidates.create(name: 'Jon Snow', age: 20,
+    biography: "Lord Snow, The Bastard of Winterfell.",
+    image: File.new('./spec/fixures/jon-snow.jpeg'),
+    political_office: 'congressman' )
+stark.candidates.create(name: 'Catelyn Tully', age: 35,
+    biography: "Catelyn Tully, or Catelyn Stark, is the wife of Lord Eddard Stark and Lady of Winterfell.",
+    image: File.new('./spec/fixures/catelyn.jpg'),
+    political_office: 'state_representative' )
+
+PoliticalParty.create(name: 'House Greyjoy').candidates
+  .create(name: 'Theon Greyjoy', age: 25,
+    biography: "Prince of Winterfell; Captain of Sea Bitch; Lord of the Iron Islands",
+    image: File.new('./spec/fixures/theon.jpg'),
+    political_office: 'city_councilman' )
+
+PoliticalParty.create(name: 'House Tarth').candidates
+  .create(name: 'Brienne of Tarth', age: 30,
+    biography: "The Maid of Tarth; Brienne the Beauty.",
+    image: File.new('./spec/fixures/brienne.jpg'),
+    political_office: 'state_representative' )
