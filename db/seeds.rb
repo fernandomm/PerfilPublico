6d6b47356a9de1d821bac5ea1f298f950586c8b9
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-19 19:12:37 -0200

    finishing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,67 +29,102 @@ puts 'CREATED ADMIN USER: ' << user.email
 #   end
 # end
 
+ActiveRecord::Base.transaction do
+  puts "Seeding Candidates and its Political Parties..."
+  # Seeds for the competition
+  PoliticalParty.create(name: 'House Targaryen').candidates
+    .create(name: 'Daenerys Targaryen', age: 18, biography: """
+      Daenerys of the House Targaryen, the First of Her Name;
+      Queen of Meereen;
+      Queen of the Andals, the Rhoynar and the First Men;
+      Lady Regnant of the Seven Kingdoms;
+      Protector of the Realm;
+      Khaleesi of the Great Grass Sea;
+      Breaker of Chains;
+      Mother of Dragons.
+      """, image: File.new('./spec/fixures/daenerys.jpg'),
+      political_office: 'president' )
 
-# Seeds for the competition
-PoliticalParty.create(name: 'House Targaryen').candidates
-  .create(name: 'Daenerys Targaryen', age: 18, biography: """
-    Daenerys of the House Targaryen, the First of Her Name;
-    Queen of Meereen;
-    Queen of the Andals, the Rhoynar and the First Men;
-    Lady Regnant of the Seven Kingdoms;
-    Protector of the Realm;
-    Khaleesi of the Great Grass Sea;
-    Breaker of Chains;
-    Mother of Dragons.
-    """, image: File.new('./spec/fixures/daenerys.jpg'),
-    political_office: 'president' )
+  baratheonn = PoliticalParty.create(name: 'House Baratheonn')
+  baratheonn.candidates.create(name: 'Joffrey Baratheonn', age: 19, biography: """
+      King Joffrey of House Baratheon, the First of His Name.
+      The King on the Iron Throne.
+      """, image: File.new('./spec/fixures/joffrey.jpg'),
+      political_office: 'president' )
+  baratheonn.candidates.create(name: 'Tommen Baratheon', age: 12,
+      biography: "Tommen of the House Baratheon the First of His Name",
+      image: File.new('./spec/fixures/tommen.jpg'),
+      political_office: 'city_councilman' )
 
-baratheonn = PoliticalParty.create(name: 'House Baratheonn')
-baratheonn.candidates.create(name: 'Joffrey Baratheonn', age: 19, biography: """
-    King Joffrey of House Baratheon, the First of His Name.
-    The King on the Iron Throne.
-    """, image: File.new('./spec/fixures/joffrey.jpg'),
-    political_office: 'president' )
-baratheonn.candidates.create(name: 'Tommen Baratheon', age: 12,
-    biography: "Tommen of the House Baratheon the First of His Name",
-    image: File.new('./spec/fixures/tommen.jpg'),
-    political_office: 'city_councilman' )
-
-lannister = PoliticalParty.create(name: 'House Lannister')
-lannister.candidates.create(name: 'Tyrion Lannister', age: 36, biography: """
-    Intelligent, witty and well-read, and shares his father's skill for business
-    and political maneuvering.
-    """, image: File.new('./spec/fixures/tyrion.jpg'),
-    political_office: 'senator')
-lannister.candidates.create(name: 'Cersei Lannister', age: 40, biography: """
-    Queen Regent; Lady Paramount of the Westerlands; Lady of Casterly Rock.
-    """, image: File.new('./spec/fixures/cercei.png'),
-    political_office: 'senator')
+  lannister = PoliticalParty.create(name: 'House Lannister')
+  lannister.candidates.create(name: 'Tyrion Lannister', age: 36, biography: """
+      Intelligent, witty and well-read, and shares his father's skill for business
+      and political maneuvering.
+      """, image: File.new('./spec/fixures/tyrion.jpg'),
+      political_office: 'senator')
+  lannister.candidates.create(name: 'Cersei Lannister', age: 40, biography: """
+      Queen Regent; Lady Paramount of the Westerlands; Lady of Casterly Rock.
+      """, image: File.new('./spec/fixures/cercei.png'),
+      political_office: 'senator')
 
 
-PoliticalParty.create(name: 'House Tyrell').candidates
-  .create(name: 'Olenna Tyrell', age: 90, biography: "The Queen of Thorns.",
-    image: File.new('./spec/fixures/olenna.jpg'),
-    political_office: 'congressman' )
+  PoliticalParty.create(name: 'House Tyrell').candidates
+    .create(name: 'Olenna Tyrell', age: 90, biography: "The Queen of Thorns.",
+      image: File.new('./spec/fixures/olenna.jpg'),
+      political_office: 'congressman' )
 
-stark = PoliticalParty.create(name: 'House Stark');
-stark.candidates.create(name: 'Jon Snow', age: 20,
-    biography: "Lord Snow, The Bastard of Winterfell.",
-    image: File.new('./spec/fixures/jon-snow.jpeg'),
-    political_office: 'congressman' )
-stark.candidates.create(name: 'Catelyn Tully', age: 35,
-    biography: "Catelyn Tully, or Catelyn Stark, is the wife of Lord Eddard Stark and Lady of Winterfell.",
-    image: File.new('./spec/fixures/catelyn.jpg'),
-    political_office: 'state_representative' )
+  stark = PoliticalParty.create(name: 'House Stark');
+  stark.candidates.create(name: 'Jon Snow', age: 20,
+      biography: "Lord Snow, The Bastard of Winterfell.",
+      image: File.new('./spec/fixures/jon-snow.jpeg'),
+      political_office: 'congressman' )
+  stark.candidates.create(name: 'Catelyn Tully', age: 35,
+      biography: "Catelyn Tully, or Catelyn Stark, is the wife of Lord Eddard Stark and Lady of Winterfell.",
+      image: File.new('./spec/fixures/catelyn.jpg'),
+      political_office: 'state_representative' )
 
-PoliticalParty.create(name: 'House Greyjoy').candidates
-  .create(name: 'Theon Greyjoy', age: 25,
-    biography: "Prince of Winterfell; Captain of Sea Bitch; Lord of the Iron Islands",
-    image: File.new('./spec/fixures/theon.jpg'),
-    political_office: 'city_councilman' )
+  PoliticalParty.create(name: 'House Greyjoy').candidates
+    .create(name: 'Theon Greyjoy', age: 25,
+      biography: "Prince of Winterfell; Captain of Sea Bitch; Lord of the Iron Islands",
+      image: File.new('./spec/fixures/theon.jpg'),
+      political_office: 'city_councilman' )
 
-PoliticalParty.create(name: 'House Tarth').candidates
-  .create(name: 'Brienne of Tarth', age: 30,
-    biography: "The Maid of Tarth; Brienne the Beauty.",
-    image: File.new('./spec/fixures/brienne.jpg'),
-    political_office: 'state_representative' )
+  PoliticalParty.create(name: 'House Tarth').candidates
+    .create(name: 'Brienne of Tarth', age: 30,
+      biography: "The Maid of Tarth; Brienne the Beauty.",
+      image: File.new('./spec/fixures/brienne.jpg'),
+      political_office: 'state_representative' )
+end
+
+ActiveRecord::Base.transaction do
+  puts "Seeding Categories..."
+  %w(roads safety health castles).each do |category|
+    Category.create(name: category)
+  end
+end
+
+puts "Seeding promises..."
+Candidate.all.each do |candidate|
+  ActiveRecord::Base.transaction do
+    puts "Seeding promises for #{candidate.name}..."
+    7.times do |x|
+      text = []
+      url = URI.parse("http://www.gameofipsum.com")
+      res = Net::HTTP.start(url.host, url.port) do |http|
+        http.get("/api/?type=json&paragraphs=1")
+      end
+      text = JSON.parse res.body
+      promise = candidate.promises.new(title: "#{text[0][0..15]}...",
+        description: text[0], category: Category.order("RANDOM()").first,
+        visible: true, date: (1..500).to_a.sample.days.ago,
+        up_votes: (1..500).to_a.sample, down_votes: (1..500).to_a.sample,
+        visualization_count: (1..500).to_a.sample,
+        status: ['accomplished', 'in_progress', 'not_accomplished'][(0..2).to_a.sample])
+      begin
+        promise.save!
+      rescue Exception => e
+        puts e.inspect
+      end
+    end
+  end
+end
