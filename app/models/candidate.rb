1fa4b900dc2475bec8a4c838cd04d95d014862de
Ivan Santos <pragmaticivan@gmail.com>
2014-10-19 13:36:31 -0300

    Base of state

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -8,7 +8,44 @@ class Candidate < ActiveRecord::Base
 
   mount_uploader :image, CandidateImageUploader
 
+  def state_list
+    [["Acre", "ac"],
+    ["Alagoas", "al"],
+    ["Amapá", "ap"],
+    ["Amazonas", "am"],
+    ["Bahia", "ba"],
+    ["Ceará", "ce"],
+    ["Distrito Federal", "df"],
+    ["Espírito Santo", "es"],
+    ["Goiás", "go"],
+    ["Maranhão", "ma"],
+    ["Mato Grosso", "mt"],
+    ["Mato Grosso do Sul", "ms"],
+    ["Minas Gerais", "mg"],
+    ["Pará", "pa"],
+    ["Paraíba", "pb"],
+    ["Paraná", "pr"],
+    ["Pernambuco", "pe"],
+    ["Piauí", "pi"],
+    ["Rio de Janeiro", "rj"],
+    ["Rio Grande do Norte", "rn"],
+    ["Rio Grande do Sul", "rs"],
+    ["Rondônia", "ro"],
+    ["Roraima", "rr"],
+    ["Santa Catarina", "sc"],
+    ["São Paulo", "sp"],
+    ["Sergipe", "se"],
+    ["Tocantins", "to"]]
+  end
+
   def political_office_enum
+    # [["Mayor","mayor"],
+    #  ["City Councilman","city_councilman"],
+    #  ["State Representative","state_representative"],
+    #  ["Governor","governor"],
+    #  ["Congressman","congressman"],
+    #  ["Senator","senator"],
+    #  ["President","president"]]
     ['mayor', 'city_councilman', 'state_representative', 'governor', 'congressman', 'senator', 'president']
   end
 
