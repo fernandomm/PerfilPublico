bc8fcee96287de5d1d53e840f5cefc745efdd1b2
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-27 23:30:07 -0200

    improving code climate

--- app/controllers/promises_controller.rb
+++ app/controllers/promises_controller.rb
@@ -3,13 +3,11 @@ class PromisesController < ApplicationController
 
   def new
     @candidate = Candidate.find params[:candidate_id]
-
     @promise = @candidate.promises.build
   end
 
   def create
     @candidate = Candidate.find params[:candidate_id]
-
     @promise = @candidate.promises.build(promise_params)
     @promise.visible = false
 
@@ -22,42 +20,35 @@ class PromisesController < ApplicationController
 
   def show
     @candidate = Candidate.find params[:candidate_id]
-
     @promise = @candidate.promises.find(params[:id])
     @promise.increment!(:visualization_count)
   end
 
   def vote_up
-    @candidate = Candidate.find params[:candidate_id]
-
-    @promise = @candidate.promises.find(params[:id])
-
-    if session.has_key?(@promise.id)
-      redirect_to candidate_promise_path(@candidate, @promise), :alert => I18n.t('promises.already_voted')
-    else
-      @promise.increment!(:up_votes)
-      register_vote_in_session
-
-      redirect_to candidate_promise_path(@candidate, @promise), :notice => I18n.t('promises.vote_registered')
-    end
+    vote(:up_votes)
   end
 
   def vote_down
-    @candidate = Candidate.find params[:candidate_id]
-
-    @promise = @candidate.promises.find(params[:id])
-
-    if session.has_key?(@promise.id)
-      redirect_to candidate_promise_path(@candidate, @promise), :alert => I18n.t('promises.already_voted')
-    else
-      @promise.increment!(:down_votes)
-      register_vote_in_session
-
-      redirect_to candidate_promise_path(@candidate, @promise), :notice => I18n.t('promises.vote_registered')
-    end
+    vote(:down_votes)
   end
 
   private
+
+    def vote(type)
+      @candidate = Candidate.find params[:candidate_id]
+      @promise = @candidate.promises.find(params[:id])
+
+      if session.has_key?(@promise.id)
+        redirect_to candidate_promise_path(@candidate, @promise),
+          :alert => I18n.t('promises.already_voted')
+      else
+        @promise.increment!(type)
+        register_vote_in_session
+        redirect_to candidate_promise_path(@candidate, @promise),
+          :notice => I18n.t('promises.vote_registered')
+      end
+    end
+
     def promise_params
       params.require(:promise).permit(:title, :description, :category_id, :date, :status)
     end
