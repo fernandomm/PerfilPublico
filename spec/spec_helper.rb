4709bc944ab02f1e1a871e7798a04438fa95f2e8
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-27 22:50:24 -0200

    houndci fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'coveralls'
+require "coveralls"
 Coveralls.wear!
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
